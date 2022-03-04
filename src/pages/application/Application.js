3b5509913cdfa23d44b00f1af10f762c2744e20f
riyababum <89964735+riyababum@users.noreply.github.com>
2022-03-04 12:00:31 +0530

    Add files via upload

--- src/pages/application/Application.js
+++ src/pages/application/Application.js
@@ -1,170 +1,168 @@
 import React,{useState} from 'react';
 import { useNavigate } from 'react-router';
 import './Application.css'
-import {Validateapplication} from '../validateForm'
+import {Validateapplication} from '../validateForm';
+import axios from 'axios';
 
 function Application(props) {
 
-    const [applicationValues, setapplicationValues] = useState({ username: "",coursetype:"", email: "", photo: "", phoneno: "" , batch: "", startingdate: "", endingdate: ""});
-    
+    const [applicationValues, setapplicationValues] = useState({ username: "",coursetype:"", email: "",  phoneno: "" , batch: "", startingdate: "", endingdate: ""});
+    const [filename,setFileName] = useState("")   ;
     const [errorValues, setErrorValues] = useState({});
+    
 
-    const navigate =useNavigate;
+    const token =localStorage.getItem('token');
+
+    const navigate = useNavigate;
 
     const handleChange = (event) => {
-       
         const { name, value } = event.target;
         setapplicationValues({ ...applicationValues, [name]: value });
-        console.log(applicationValues)
     }
+    const dateChange = (event) => {
+        const date = event.target.value;
+        const myDate = new Date(date);
+    }
+
+    const handleChangeImage = (event) => {
+        setFileName(event.target.files[0]);
+    }
+    
     
     const handleSubmit = (event) => {
         event.preventDefault();
-        const validationErrors = Validateapplication(applicationValues);
-        console.log(validationErrors)
+
+        const formData = new FormData();
+
+        formData.append('username', applicationValues.username);
+        formData.append('coursetype', applicationValues.coursetype);
+        formData.append('email', applicationValues.email);
+        formData.append('phoneno', applicationValues.phoneno);
+        formData.append('batch', applicationValues.batch);
+        formData.append('startingdate', applicationValues.startingdate);
+        formData.append('endingdate', applicationValues.endingdate);
+        formData.append('photo', filename);
+ 
+        const validationErrors = Validateapplication(applicationValues,filename);
         setErrorValues(validationErrors);
         if(Object.keys(validationErrors).length === 0)
-            ApplicationDetails();
+            ApplicationDetails(formData);
     }
     
-    const ApplicationDetails = async () => {
-        const username = applicationValues.username;
-        const coursetype = applicationValues.coursetype;
-        const email = applicationValues.email;
-        // const photo = applicationValues.photo;
-        const phoneno = applicationValues.phoneno;
-        const batch = applicationValues.batch;
-        const startingdate = applicationValues.startingdate;
-        const endingdate = applicationValues.endingdate;
-        
-        const response = await fetch("/api/user/application", {
-            method: 'post',
-            body: JSON.stringify({ username,coursetype, email,phoneno,batch,startingdate,endingdate }),
-            headers: {
-                'Content-Type': 'application/json'
+    const ApplicationDetails = async (formData) => {
+        axios.post("/api/application/postapplication", formData,
+            {headers: {
+                'Content-Type': 'application/json',
+                'authorization': 'JWT '+ token
+                }
+            }).then(res=>{
+                console.log(res.data);
+                if (res.status) {
+                        alert("Application submitted successfully");
+                        navigate("/status", { replace: true });
+                    } else {
+                        alert("Application applied Unsuccessful!");
+                    }})
             }
-        })
-        const body = await response.json();
-            
-        if (body.status === "Success") {
-            alert("Application applyed Successfully");
-            navigate("/status", { replace: true });
-        } else {
-            alert("Application applyed Unsuccessful!");
-        }
-    };
-
 
 
     return (
-      <div className='applicationbody'>
-          <img src='https://www.ajce.in/cse/images/ict_academy.png' className='applicationimage' alt='ictk'></img> 
-        <div className='application'>  
-                
-				<form onSubmit={handleSubmit} className='applicationform'>
+        <div className='applicationbody'>
+            <img src='https://www.ajce.in/cse/images/ict_academy.png' className='applicationimage' alt='ictk'></img> 
 
+            <div className='application'>  
+                
+			    <form onSubmit={(e)=>handleSubmit(e)} className='applicationform' encType='multipart/form-data'>
 					<label id='applicationtitle'>Application  </label><br></br>
 
-
 				    <div className='row'>
-                    <div className='col-25'>	
-                    <label>Name:</label> 
-                    </div>
-                    <div className='col-75'>
-                    <input  placeholder='Name' name="username"  value={applicationValues.username} onChange={handleChange} /> 
-                    </div>
-                    <p className="applicationerrorText">{errorValues.username}</p><br></br>
-                    </div>
-
-
-                    <label>Course type:</label>    
-                        <select name="coursetype" id="coursetype"  value={applicationValues.coursetype} onChange={handleChange}>
-                        <option value=""> select Course Type</option>
-                        <option > FULL STACK DEVELOPMENT </option>                       
-                        <option > SOFTWARE TESTING </option>
-                        <option > CYBER SECURITY ANALYST </option>
-                        </select> 
-                        <p className="applicationerrorText">{errorValues.coursetype}</p><br></br>
-
-                    
-                   
-                   
-                    <div className='row'>
-                    <div className='col-25'>	
-                    <label>Email:</label>
-                    </div>
-                    <div className='col-75'>
-                    <input  placeholder='Email' name="email"  value={applicationValues.email} onChange={handleChange}/> 
-                    </div>
-                    <p className="applicationerrorText">{errorValues.email}</p><br></br>
+                        <div className='col-25'>	
+                            <label>Name:</label> 
+                        </div>
+                        <div className='col-75'>
+                            <input  placeholder='Name' name="username"  value={applicationValues.username} onChange={handleChange} /> 
+                        </div>
+                        <p className="applicationerrorText">{errorValues.username}</p><br></br>
                     </div>
 
 
-                    
+                    <label htmlFor='coursetype'>Course type:</label>    
+                    <select name="coursetype" id="coursetype"  value={applicationValues.coursetype} onChange={handleChange}>
+                        <option value="" disabled> --Select Course Type--</option>
+                        <option>Full Stack Development</option>                       
+                        <option>Software Development</option>
+                        <option>Cyber Security Analyst</option>
+                        <option>Robotic Process Automation</option>
+                        <option>Data Science and Analytics</option>
+                        <option>Digital Marketing</option>
+                    </select> 
+                    <p className="applicationerrorText">{errorValues.coursetype}</p><br></br>
 
                     <div className='row'>
-                    <div className='col-25'>	
-                    <label>Select Image:</label>
-                    </div>
-                    <div className='col-75'>
-                    <input type='file' placeholder='photo' name="photo"  value={applicationValues.photo} onChange={handleChange}/>
-                    </div>
-                    <p className="applicationerrorText">{errorValues.photo}</p><br></br>
-                    </div>
+                        <div className='col-25'>	
+                            <label>Email:</label>
+                        </div>
+                        <div className='col-75'>
+                            <input  placeholder='Email' name="email"  value={applicationValues.email} onChange={handleChange}/> 
+                        </div>
+                        <p className="applicationerrorText">{errorValues.email}</p><br></br>
+                    </div>              
 
-
-
+                    <div className='row'>
+                        <div className='col-25'>	
+                            <label>Select Image:</label>
+                        </div>
+                        <div className='col-75'>
+                            <input type='file' placeholder='photo' filename= 'photo' onChange={handleChangeImage}/>
+                        </div>
+                        <p className="applicationerrorText">{errorValues.photo}</p><br></br>
+                    </div>
+ 
 				    <div className='row'>
-                    <div className='col-25'>	
-                    <label>Phone No:</label>
+                        <div className='col-25'>	
+                            <label>Phone No:</label>
+                        </div>
+                        <div className='col-75'>
+                            <input  placeholder='Phone No' name="phoneno"  value={applicationValues.phoneno} onChange={handleChange}/>
+                        </div>
+                        <p className="applicationerrorText">{errorValues.phoneno}</p><br></br>
                     </div>
-                    <div className='col-75'>
-                    <input  placeholder='Phone No' name="phoneno"  value={applicationValues.phoneno} onChange={handleChange}/>
-                    </div>
-                    <p className="applicationerrorText">{errorValues.phoneno}</p><br></br>
-                    </div>
-
-
 
                     <div className='row'>
-                    <div className='col-25'>	
-                    <label>Batch:</label>
+                        <div className='col-25'>	
+                            <label>Batch:</label>
+                        </div>
+                        <div className='col-75'>
+                            <input  placeholder='Batch' name="batch"  value={applicationValues.batch} onChange={handleChange}/> 
+                        </div>
+                        <p className="applicationerrorText">{errorValues.batch}</p><br></br>
                     </div>
-                    <div className='col-75'>
-                    <input  placeholder='Batch' name="batch"  value={applicationValues.batch} onChange={handleChange}/> 
-                    </div>
-                    <p className="applicationerrorText">{errorValues.batch}</p><br></br>
-                    </div>
-
 
                     <div className='row'>
-                    <div className='col-25'>	
-                    <label>Course Starting date:</label>
+                        <div className='col-25'>	
+                            <label>Course Starting date:</label>
+                        </div>
+                        <div className='col-75'>
+                            <input type='date' placeholder='Course Starting date' name="startingdate"  value={applicationValues.startingdate} onChange={(e)=>{handleChange(e); dateChange(e);}}/> 
+                        </div>
+                        <p className="applicationerrorText">{errorValues.startingdate} </p><br></br>
                     </div>
-                    <div className='col-75'>
-                    <input  placeholder='Course Starting date' name="startingdate"  value={applicationValues.startingdate} onChange={handleChange}/> 
-                    </div>
-                    <p className="applicationerrorText">{errorValues.startingdate} </p><br></br>
-                    </div>
-
-
 
                     <div className='row'>
-                    <div className='col-25'>	
-                    <label>Course Ending Date:</label>
+                        <div className='col-25'>	
+                            <label>Course Ending Date:</label>
+                        </div>
+                        <div className='col-75'>
+                            <input type='date' placeholder='Course Ending Date' name="endingdate"  value={eDate} onChange={handleChange}/> 
+                        </div>
+                        <p className="applicationerrorText">{errorValues.endingdate} </p><br></br>
                     </div>
-                    <div className='col-75'>
-                    <input  placeholder='Course Ending Date' name="endingdate"  value={applicationValues.endingdate} onChange={handleChange}/> 
-                    </div>
-                    <p className="applicationerrorText">{errorValues.endingdate} </p><br></br>
-                    </div>
-
 
 					<button  className='submitbutton'>Submit</button>
 
 				</form>		
-        </div>
-      </div>  
+            </div>
+        </div>  
     );
 }
 
