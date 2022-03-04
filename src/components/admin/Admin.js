d047d7ce1ae010c39dd7741c48fa50a5df1b5273
Anisha-sunny <93588867+Anisha-sunny@users.noreply.github.com>
2022-03-04 15:22:15 +0530

    Add files via upload

---
+++ src/components/admin/Admin.js
@@ -0,0 +1,27 @@
+import React from 'react';
+import { Link } from 'react-router-dom';
+import "./Admin.css"
+
+function Admin(props) {
+    return (
+        <div>
+            
+            <nav className="adheader">
+               
+                <div className='adsidenav'>
+
+                <img src='https://ictkerala.org/wp-content/uploads/2019/01/cropped-ict-ico.png' alt="Hlogo" id='adheaderlogo'></img>
+
+                    <Link className="link" to="/admin"><br/>Home</Link>
+                    <Link className="link" to="/batch">Batch Managers</Link>
+                    <Link className="link" to="/course">Course List</Link>
+                    <Link className="link" to="/">Logout</Link>
+    
+                </div>
+            </nav>
+     
+        </div>
+    );
+}
+
+export default Admin;
