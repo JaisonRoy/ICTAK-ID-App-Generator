5799ce8d49ab61adac741e77fbf0a745fb923dc8
aasishkrishnan <93522715+aasishkrishnan@users.noreply.github.com>
2022-03-13 23:07:24 +0530

    Update CourseList.js

--- src/components/courselist/CourseList.js
+++ src/components/courselist/CourseList.js
@@ -1,64 +1,8 @@
 /* eslint-disable jsx-a11y/anchor-is-valid */
+
 import React from 'react';
 import "./CourseList.css"
 import "bootstrap/dist/css/bootstrap.min.css"
-
-import $ from 'jquery';
-                                                                    //  I've install Jquery and Bootstrap to this project using npm installer, so please install that before use
- 
-                                                                    //    There is a error in this portion from js:10 to js:47 (so please dubug before use) {Main problem is with add and save button}, I think it's a small problem.
-// $(document).on("ready", function(){
-// 	$('[data-toggle="tooltip"]').tooltip();
-// 	var actions = $("table td:last-child").html();
-// 	// Append table with add row form on add new button click
-//     $(".add-new").on("click", function(){
-// 		$(this).attr("disabled", "disabled");
-// 		var index = $("table tbody tr:last-child").index();
-//         var row = '<tr>' +
-//             '<td><input type="text" className="form-control" name="name" id="name"></td>' +
-//             '<td><input type="text" className="form-control" name="department" id="department"></td>' +
-//             '<td><input type="text" className="form-control" name="phone" id="phone"></td>' +
-// 			'<td>' + actions + '</td>' +
-//         '</tr>';
-//     	$("table").append(row);		
-// 		$("table tbody tr").eq(index + 1).find(".add, .edit").toggle();
-//         $('[data-toggle="tooltip"]').tooltip();
-//     });
-// 	// Add row on add button click
-// 	$(document).on("click", ".add", function(){
-// 		var empty = false;
-// 		var input = $(this).parents("tr").find('input[type="text"]');
-//         input.each(function(){
-// 			if(!$(this).val()){
-// 				$(this).addClass("error");
-// 				empty = true;
-// 			} else{
-//                 $(this).removeClass("error");
-//             }
-// 		});
-// 		$(this).parents("tr").find(".error").first().on("focus");
-// 		if(!empty){
-// 			input.each(function(){
-// 				$(this).parent("td").html($(this).val());
-// 			});			
-// 			$(this).parents("tr").find(".add, .edit").toggle();
-// 			$(".add-new").removeAttr("disabled");
-// 		}		
-//     });
-	// Edit row on edit button click
-	$(document).on("click", ".edit", function(){		
-        $(this).parents("tr").find("td:not(:last-child)").each(function(){
-			$(this).html('<input type="text" class="form-control" value="' + $(this).text() + '">');
-		});		
-		$(this).parents("tr").find(".add, .edit").toggle();
-		$(".add-new").attr("disabled", "disabled");
-    });
-	// Delete row on delete button click
-	$(document).on("click", ".delete", function(){
-        $(this).parents("tr").remove();
-		$(".add-new").removeAttr("disabled");
-    });
-// });
 
 
 function CourseList(props) {
@@ -92,7 +36,7 @@ function CourseList(props) {
                 </thead>
                 <tbody>
                     <tr>
-                        <td>FULL STACK DEVELOPMENT</td>
+                        <td>Bsc Physics</td>
                        
                         <td>
                             <a className="add" title="Add" data-toggle="tooltip"><i className="material-icons">&#xE03B;</i></a>
@@ -101,7 +45,7 @@ function CourseList(props) {
                         </td>
                     </tr>
                     <tr>
-                        <td>SOFTWARE TESTING </td>
+                        <td>Bsc Chemistry</td>
                       
                         <td>
                             <a className="add" title="Add" data-toggle="tooltip"><i className="material-icons">&#xE03B;</i></a>
@@ -110,16 +54,7 @@ function CourseList(props) {
                         </td>
                     </tr>
                     <tr>
-                        <td>DATA SCIENCE & ANALYTICS</td>
-                      
-                        <td>
-                            <a className="add" title="Add" data-toggle="tooltip"><i className="material-icons">&#xE03B;</i></a>
-                            <a className="edit" title="Edit" data-toggle="tooltip"><i className="material-icons">&#xE254;</i></a>
-                            <a className="delete" title="Delete" data-toggle="tooltip"><i className="material-icons">&#xE872;</i></a>
-                        </td>
-                    </tr>
-                    <tr>
-                        <td> CYBER SECURITY ANALYST</td>
+                        <td>BA English</td>
                        
                         <td>
                             <a className="add" title="Add" data-toggle="tooltip"><i className="material-icons">&#xE03B;</i></a>
